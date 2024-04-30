8e9ad46342f66338ae969183a10a193a0e5f79f6
Jan0007 <94890470+Jan0007@users.noreply.github.com>
2024-04-30 21:16:34 +0200

    homepage-semidone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,9 @@
         <div class="fixed w-screen h-[600px] top-0 m-0 z-0 sm:scale-0 lg:scale-100">
             <TresCanvas clear-color="#8EA8C3">
                 <TresPerspectiveCamera />
-                <TresMesh ref="boxRef" :position="[positionX, 1, positionZ]"
-                    v-bind:style="{ transform: `translateX(${positionX}px)` }">
+                <TresMesh ref="boxRef" :position="[positionX, positionY, positionZ]">
                     <TresBoxGeometry />
-                    <TresMeshNormalMaterial />
+                    <TresMeshBasicMaterial :color="'#161925'" />
                 </TresMesh>
             </TresCanvas>
         </div>
@@ -40,21 +39,48 @@
                     volutpat ante, in pharetra metus odio a lectus. </p>
             </div>
             <div class="get-app-text">
-                <h1 id="get-app" class="text-end font-mono text-[40px] w-[50%] mr-20">
+                <h1 id="get-app" class="text-end font-mono text-[40px] mr-20">
                     Get the app
                 </h1>
-                <p class="text-end w-[50%] mr-20 mt-10">
-                    We have a windows desktop application availible for download. You can download it from the link below.
+                <p class="text-end mr-20 mt-10">
+                    We have a windows desktop application availible for download. You can download it from the link
+                    below.
                 </p>
+                <div class="border-first border-b-2 w-[50%] mr-20 mt-10"></div>
                 <div class="relative flex flex-row w-[100%] h-16 justify-end">
-                    <button class="z-20 mt-10 mr-36 button-first">Get app</button>
+                    <button class="z-20 mt-10 mr-36 button-get-app">Get app</button>
                 </div>
             </div>
-            <div class="get-app-text">
-                <h1 id="accounts" class="text-end font-mono text-[40px] w-[50%] mr-20">
-                    Get the app
+            <div class="account-text">
+                <div class="relative mx-auto">
+                    <h1 id="accounts" class="text-start font-mono text-[40px]">
+                        Create an account
+                    </h1>
+                    <p>
+                        You can create an account by clicking the button below.
+                    </p>
+
+                    <button class="create-account-button">
+                        Create an account
+                    </button>
+                </div>
+                <div class="relative mx-auto">
+                    <h2 id="#" class="text-start font-mono text-[40px]">
+                        Log into an account
+                    </h2>
+                    <p>
+                        You can log into your account by clicking the button below.
+                    </p>
+                    <button class="log-in-button">
+                        Log in
+                    </button>
+                </div>
+            </div>
+            <div class="other-projects-text">
+                <h1 id="other-projects" class="font-mono text-[40px] w-[50%] mr-20 text-center">
+                    Our other projects
                 </h1>
-                <p class="text-end w-[50%] mr-20">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cras pede
+                <p class="w-[50%] mr-20 text-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cras pede
                     libero, dapibus
                     nec,
                     pretium sit
@@ -90,6 +116,7 @@ const currentSection = ref('');
 
 const positionX = ref(3);
 const positionZ = ref(0);
+const positionY = ref(1);
 
 const { onLoop } = useRenderLoop();
 
@@ -108,6 +135,7 @@ onMounted(() => {
             if (entry.intersectionRatio > 0) {
                 const value = entry.target.getAttribute('id');
                 currentSection.value = value;  
+                console.log(currentSection.value)
             }
         });
     });
@@ -140,14 +168,30 @@ function doSomething(scrollPos) {
         if (scrollPos < 10 && scrollPos > 0) {
             positionX.value = 3 + scrollPos / 270;
             positionZ.value = 0 + scrollPos / 270;
-            console.log(positionX.value, positionZ.value)
         } else {
             positionX.value = 3 - scrollPos / 270;
             positionZ.value = 0 + scrollPos / 270;
         }
-    } else {
+    } else if (scrollPos > 878 && scrollPos < 1342) {
         positionX.value = -0.2259259259259258;
         positionZ.value = 3.225925925925926;
+    } else if (scrollPos > 1342 && scrollPos < 2000) {
+        if (scrollPos < 2000 && scrollPos > 1342) {
+            positionX.value = -0.2259259259259258 + (scrollPos - 1342) / 400;
+            positionZ.value = 3.225925925925926 - (scrollPos - 1342) / 400;
+        } else {
+            positionX.value = -0.2259259259259258 - (scrollPos - 1342) / 400;
+            positionZ.value = 3.225925925925926 + (scrollPos - 1342) / 400;
+        }
+    } else if (scrollPos > 2000 && scrollPos < 3030) {
+        positionX.value = 1.3040740740740742;
+        positionZ.value = 1.6959259259259258;
+    } else if (scrollPos > 3030) {
+        if (scrollPos < 3030 && scrollPos > 2000) {
+            positionY.value = 1 - (scrollPos - 3030) / 300;
+        } else {
+            positionY.value = 1 + (scrollPos - 3030) / 300;
+        }
     }
 }
 
